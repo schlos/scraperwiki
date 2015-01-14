0ecc513d720af761ac914f1e8bb69f174cd54355
Miroslav <schlos@users.noreply.github.com>
2015-01-14 08:44:31 +0100

    Fork of code from ScraperWiki at https://classic.scraperwiki.com/scrapers/scraperwiki/

---
+++ scraper.py
@@ -0,0 +1,9 @@
+###############################################################################
+# START HERE: Tutorial 1: Getting used to the ScraperWiki editing interface.
+# Based on tutorial 1
+###############################################################################
+
+import scraperwiki
+html = scraperwiki.scrape('http://data.gov')
+print html
+
